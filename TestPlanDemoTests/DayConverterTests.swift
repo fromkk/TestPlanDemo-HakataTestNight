feee2b3311ff06ced3d0b14b6fc3c0b2983c1ad9
Kazuya Ueoka <info@fromkk.info>
2020-01-12 15:23:41 +0900

    fixed test

--- TestPlanDemoTests/DayConverterTests.swift
+++ TestPlanDemoTests/DayConverterTests.swift
@@ -11,10 +11,18 @@ import XCTest
 
 final class DayConverterTests: XCTestCase {
     func testConvertEn() {
-        XCTAssertEqual("1st", DayConverter.convert(day: 1, for: .init(identifier: "en_US")))
+        XCTAssertEqual("1st", DayConverter.convert(day: 1, for: Locale(identifier: "en_US")))
+        XCTAssertEqual("2nd", DayConverter.convert(day: 2, for: Locale(identifier: "en_US")))
+        XCTAssertEqual("3rd", DayConverter.convert(day: 3, for: Locale(identifier: "en_US")))
+        XCTAssertEqual("4th", DayConverter.convert(day: 4, for: Locale(identifier: "en_US")))
+        XCTAssertEqual("9th", DayConverter.convert(day: 9, for: Locale(identifier: "en_US")))
+        XCTAssertEqual("11th", DayConverter.convert(day: 11, for: Locale(identifier: "en_US")))
+        XCTAssertEqual("12th", DayConverter.convert(day: 12, for: Locale(identifier: "en_US")))
+        XCTAssertEqual("13th", DayConverter.convert(day: 13, for: Locale(identifier: "en_US")))
     }
 
     func testConvertJa() {
-        XCTAssertEqual("1", DayConverter.convert(day: 1, for: .init(identifier: "ja_JP")))
+        XCTAssertEqual("1", DayConverter.convert(day: 1, for: Locale(identifier: "ja_JP")))
+        XCTAssertEqual("2", DayConverter.convert(day: 2, for: Locale(identifier: "ja_JP")))
     }
 }
